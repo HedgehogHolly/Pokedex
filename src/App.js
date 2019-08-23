d0a6795de7ebbdc50c04404e797cce3b72b10b5e
HedgehogHolly <holly@neontribe.co.uk>
2019-08-23 16:57:07 +0100

    add app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,84 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import PokemonCard from "./components/PokemonCard";
+import pokemons from "./pokemon.json";
+import styled from "styled-components";
+import uniq from "lodash/uniq";
+import flatten from "lodash/flatten";
+import upperFirst from "lodash/upperFirst";
+
+const PokemonWrapper = styled.section`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+`;
 
 function App() {
+  const [region, setRegion] = React.useState();
+  const [type, setType] = React.useState();
+
+  const availableRegions = uniq(pokemons.map(pokemon => pokemon.region));
+
+  const availableTypes = uniq(flatten(pokemons.map(pokemon => pokemon.types)));
+
+  const filterByRegion = pokemon => {
+    if (!region) {
+      return true;
+    }
+    return pokemon.region === region;
+  };
+
+  const filterByType = pokemon => {
+    if (!type) {
+      return true;
+    }
+    return pokemon.types.includes(type);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Pokédex</h1>
       </header>
+      <main>
+        <fieldset>
+          <legend>Filters or something</legend>
+          <label>
+            Region:
+            <select onChange={event => setRegion(event.target.value)}>
+              <option value="">All</option>
+              {availableRegions.map(region => (
+                <option value={region}>{region}</option>
+              ))}
+            </select>
+          </label>
+          <label>
+            Type:
+            <select onChange={event => setType(event.target.value)}>
+              <option value="">All</option>
+              {availableTypes.map(type => (
+                <option value={type}>{upperFirst(type)}</option>
+              ))}
+            </select>
+          </label>
+        </fieldset>
+        <div>
+          <PokemonWrapper>
+            {pokemons
+              .filter(filterByRegion)
+              .filter(filterByType)
+              .map(pokemon => (
+                <PokemonCard
+                  name={pokemon.title}
+                  number={pokemon.index}
+                  image={`./images/${pokemon.image}`}
+                  types={pokemon.types}
+                  key={pokemon.index}
+                />
+              ))}
+          </PokemonWrapper>
+        </div>
+      </main>
     </div>
   );
 }
