e2db639c8445925dcc6499f773aa3d4c79d7b7f9
HedgehogHolly <holly@neontribe.co.uk>
2019-09-13 17:52:26 +0100

    change the way to define type

--- src/components/Type.js
+++ src/components/Type.js
@@ -1,15 +1,20 @@
 import React from "react";
+import PropTypes from "prop-types";
 import styled from "styled-components";
-import "./type.css";
 
-const TypeBox = styled.div`
+const typeMap = {
+  fire: "red"
+};
+
+const Type = styled.div`
   width: 12px;
   height: 12px;
   margin: 0 2px;
+  background-color: ${props => typeMap[props.name] || "#000"};
 `;
 
-function Type(props) {
-  return <TypeBox className={props.name} />;
-}
+Type.propTypes = {
+  name: PropTypes.string.isRequired
+};
 
 export default Type;
